c2ed0ef4860ac7bfe07b1efc2797e13f828cb499
vsvai <vsvaibhavsingh5@gmail.com>
2026-07-13 22:56:29 +0530

    Fix base path to /Demo/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Basic_Robot_DashBoard_HUL/',
+  base: '/Demo/',
   server: {
     proxy: {
       '/api': {
